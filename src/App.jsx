67623162aa013f4c04ff3731e890abdbd7a3a41e
OlgaStarchenko <o9915530@gmail.com>
2026-08-16 13:29:04 +1000

    A Layout component has been added for nested routing. A Header and an Outlet have been added to the Layout. The MainPage, Catalog, and Contacts routes are organized as child routes of the Layout.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import MainPage from "./pages/MainPage";
 import Catalog from "./pages/Catalog";
 import Contacts from "./pages/Contacts";
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
-    <>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="contacts" element={<Contacts />} />
+      </Route>
+    </Routes>
   );
 }
